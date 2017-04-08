bdc461db443ee3961470eab7e0a528cda2ae8d41
Nelson Araujo <nelsona@google.com>
2017-04-08 15:21:00 -0700

    Auto generate Chef Compute module.
    Change-Id: Ifd28ffabd2bd7e269548b78ded2fcd6a20891311

---
+++ README.md
@@ -0,0 +1,337 @@
+# Google Compute Engine Cookbook
+
+This cookbook provides the built-in types and services for Chef to manage
+Google Cloud Compute resources, as native Chef types.
+
+
+## Requirements
+
+### Platforms
+
+#### Supported Operating Systems
+
+This cookbook was tested on the following operating systems:
+
+* RedHat 6, 7
+* CentOS 6, 7
+* Debian 7, 8
+* Ubuntu 12.04, 14.04, 16.04, 16.10
+* SLES 11-sp4, 12-sp2
+* openSUSE 13
+
+## Example
+
+```ruby
+
+# TODO(alexstephen): Add example here
+
+```
+
+## Credentials
+
+All Google Cloud Platform cookbooks use an unified authentication mechanism,
+provided by the `google-gauth` cookbook. Don't worry, it is automatically
+installed when you install this module.
+
+### Example
+
+```ruby
+gauth_credential 'mycred' do
+  action :serviceaccount
+  path '/home/nelsonjr/my_account.json'
+  scopes [
+    'https://www.googleapis.com/auth/ndev.clouddns.readwrite'
+  ]
+end
+
+```
+
+TODO(nelsonjr): Add location here once auth module is published:
+For complete details about the credential cookbook please visit ________
+
+## Resources
+
+* [`gcompute_address`](#gcompute_address) -
+    Represents an Address resource.
+    Each virtual machine instance has an ephemeral internal IP address and,
+    optionally, an external IP address. To communicate between instances on
+    the same network, you can use an instance's internal IP address. To
+    communicate with the Internet and instances outside of the same
+    network,
+    you must specify the instance's external IP address.
+    Internal IP addresses are ephemeral and only belong to an instance for
+    the lifetime of the instance; if the instance is deleted and recreated,
+    the instance is assigned a new internal IP address, either by Compute
+    Engine or by you. External IP addresses can be either ephemeral or
+    static.
+* [`gcompute_disk_type`](#gcompute_disk_type) -
+    Represents a DiskType resource. A DiskType resource represents the type
+    of disk to use, such as a pd-ssd or pd-standard. To reference a disk
+    type, use the disk type's full or partial URL.
+* [`gcompute_network`](#gcompute_network) -
+    Represents a Network resource.
+    Your Cloud Platform Console project can contain multiple networks, and
+    each network can have multiple instances attached to it. A network
+    allows
+    you to define a gateway IP and the network range for the instances
+    attached to that network. Every project is provided with a default
+    network
+    with preset configurations and firewall rules. You can choose to
+    customize
+    the default network by adding or removing rules, or you can create new
+    networks in that project. Generally, most users only need one network,
+    although you can have up to five networks per project by default.
+    A network belongs to only one project, and each instance can only
+    belong
+    to one network. All Compute Engine networks use the IPv4 protocol.
+    Compute
+    Engine currently does not support IPv6. However, Google is a major
+    advocate of IPv6 and it is an important future direction.
+* [`gcompute_region`](#gcompute_region) -
+    Represents a Region resource. A region is a specific geographical
+    location where you can run your resources. Each region has one or more
+    zones
+
+
+### gcompute_address
+Represents an Address resource.
+
+Each virtual machine instance has an ephemeral internal IP address and,
+optionally, an external IP address. To communicate between instances on
+the same network, you can use an instance's internal IP address. To
+communicate with the Internet and instances outside of the same network,
+you must specify the instance's external IP address.
+
+Internal IP addresses are ephemeral and only belong to an instance for
+the lifetime of the instance; if the instance is deleted and recreated,
+the instance is assigned a new internal IP address, either by Compute
+Engine or by you. External IP addresses can be either ephemeral or
+static.
+
+
+#### Example
+
+```ruby
+
+# TODO(alexstephen): Add example here
+
+```
+
+#### Actions
+
+* `create` -
+  Converges the `gcompute_address` resource into the final
+  state described within the block. If the resource does not exist, Chef will
+  attempt to create it.
+* `delete` -
+  Ensures the `gcompute_address` resource is not present.
+  If the resource already exists Chef will attempt to delete it.
+
+#### Properties
+
+* `address` -
+  The static external IP address represented by this
+  resource. Only IPv4 is supported.
+* `creation_timestamp` -
+  Creation timestamp in RFC3339 text format.
+* `description` -
+  An optional description of this resource.
+* `id` -
+  The unique identifier for the resource.
+* `name` -
+  Name of the resource.
+* `region` -
+  URL of the region where the regional address resides.
+  This field is not applicable to global addresses.
+* `users` -
+  The URLs of the resources that are using this address.
+
+### gcompute_disk_type
+Represents a DiskType resource. A DiskType resource represents the type
+of disk to use, such as a pd-ssd or pd-standard. To reference a disk
+type, use the disk type's full or partial URL.
+
+
+#### Example
+
+```ruby
+
+# TODO(alexstephen): Add example here
+
+```
+
+#### Actions
+
+* `create` -
+  Converges the `gcompute_disk_type` resource into the final
+  state described within the block. If the resource does not exist, Chef will
+  attempt to create it.
+* `delete` -
+  Ensures the `gcompute_disk_type` resource is not present.
+  If the resource already exists Chef will attempt to delete it.
+
+#### Properties
+
+* `creation_timestamp` -
+  Creation timestamp in RFC3339 text format.
+* `default_disk_size_gb` -
+  Server-defined default disk size in GB.
+* `deprecated_deleted` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to DELETED.
+* `deprecated_deprecated` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to DEPRECATED.
+* `deprecated_obsolete` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to OBSOLETE.
+* `deprecated_replacement` -
+  The URL of the suggested replacement for a deprecated resource. The
+  suggested replacement resource must be the same kind of resource as
+  the deprecated resource.
+* `deprecated_state` -
+  The deprecation state of this resource. This can be DEPRECATED,
+  OBSOLETE, or DELETED. Operations which create a new resource using a
+  DEPRECATED resource will return successfully, but with a warning
+  indicating the deprecated resource and recommending its replacement.
+  Operations which use OBSOLETE or DELETED resources will be rejected
+  and result in an error.
+* `description` -
+  An optional description of this resource.
+* `id` -
+  The unique identifier for the resource.
+* `name` -
+  Name of the resource.
+* `valid_disk_size` -
+  An optional textual description of the valid disk size, such as
+  "10GB-10TB".
+* `zone` -
+  URL of the zone where the disk type resides.
+
+### gcompute_network
+Represents a Network resource.
+
+Your Cloud Platform Console project can contain multiple networks, and
+each network can have multiple instances attached to it. A network allows
+you to define a gateway IP and the network range for the instances
+attached to that network. Every project is provided with a default network
+with preset configurations and firewall rules. You can choose to customize
+the default network by adding or removing rules, or you can create new
+networks in that project. Generally, most users only need one network,
+although you can have up to five networks per project by default.
+
+A network belongs to only one project, and each instance can only belong
+to one network. All Compute Engine networks use the IPv4 protocol. Compute
+Engine currently does not support IPv6. However, Google is a major
+advocate of IPv6 and it is an important future direction.
+
+
+#### Example
+
+```ruby
+
+# TODO(alexstephen): Add example here
+
+```
+
+#### Actions
+
+* `create` -
+  Converges the `gcompute_network` resource into the final
+  state described within the block. If the resource does not exist, Chef will
+  attempt to create it.
+* `delete` -
+  Ensures the `gcompute_network` resource is not present.
+  If the resource already exists Chef will attempt to delete it.
+
+#### Properties
+
+* `description` -
+  An optional description of this resource. Provide this property when
+  you create the resource.
+* `gateway_ipv4` -
+  A gateway address for default routing to other networks. This value is
+  read only and is selected by the Google Compute Engine, typically as
+  the first usable address in the IPv4Range.
+* `id` -
+  Unique identifier for the resource; defined by the server.
+* `ipv4_range` -
+  The range of internal addresses that are legal on this network. This
+  range is a CIDR specification, for example: 192.168.0.0/16. Provided
+  by the client when the network is created.
+* `name` -
+  Name of the resource. Provided by the client when the resource is
+  created. The name must be 1-63 characters long, and comply with
+  RFC1035. Specifically, the name must be 1-63 characters long and match
+  the regular expression [a-z]([-a-z0-9]*[a-z0-9])? which means the
+  first character must be a lowercase letter, and all following
+  characters must be a dash, lowercase letter, or digit, except the last
+  character, which cannot be a dash.
+* `subnetworks` -
+  Server-defined fully-qualified URLs for all subnetworks in this
+  network.
+* `auto_create_subnetworks` -
+  When set to true, the network is created in "auto subnet mode". When
+  set to false, the network is in "custom subnet mode".
+  In "auto subnet mode", a newly created network is assigned the default
+  CIDR of 10.128.0.0/9 and it automatically creates one subnetwork per
+  region.
+* `creation_timestamp` -
+  Creation timestamp in RFC3339 text format.
+
+### gcompute_region
+Represents a Region resource. A region is a specific geographical
+location where you can run your resources. Each region has one or more
+zones
+
+
+#### Example
+
+```ruby
+
+# TODO(alexstephen): Add example here
+
+```
+
+#### Actions
+
+* `create` -
+  Converges the `gcompute_region` resource into the final
+  state described within the block. If the resource does not exist, Chef will
+  attempt to create it.
+* `delete` -
+  Ensures the `gcompute_region` resource is not present.
+  If the resource already exists Chef will attempt to delete it.
+
+#### Properties
+
+* `creation_timestamp` -
+  Creation timestamp in RFC3339 text format.
+* `deprecated_deleted` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to DELETED.
+* `deprecated_deprecated` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to DEPRECATED.
+* `deprecated_obsolete` -
+  An optional RFC3339 timestamp on or after which the deprecation state
+  of this resource will be changed to OBSOLETE.
+* `deprecated_replacement` -
+  The URL of the suggested replacement for a deprecated resource. The
+  suggested replacement resource must be the same kind of resource as
+  the deprecated resource.
+* `deprecated_state` -
+  The deprecation state of this resource. This can be DEPRECATED,
+  OBSOLETE, or DELETED. Operations which create a new resource using a
+  DEPRECATED resource will return successfully, but with a warning
+  indicating the deprecated resource and recommending its replacement.
+  Operations which use OBSOLETE or DELETED resources will be rejected
+  and result in an error.
+* `description` -
+  An optional description of this resource.
+* `id` -
+  The unique identifier for the resource.
+* `name` -
+  Name of the resource.
+* `zones` -
+  List of zones within the region
---
+++ resources/disk_type.rb
@@ -0,0 +1,313 @@
+# Add our google/ lib
+$LOAD_PATH.unshift ::File.expand_path('../libraries', ::File.dirname(__FILE__))
+
+require 'chef/resource'
+require 'google/hash_utils'
+require 'google/property/enum'
+require 'google/property/integer'
+require 'google/property/string'
+require 'google/property/time'
+require 'google/request/get'
+require 'google/request/post'
+require 'google/request/delete'
+
+module Google
+  module GCOMPUTE
+    # A provider to manage Google Compute Engine resources.
+    # rubocop:disable Metrics/ClassLength
+    class DiskType < Chef::Resource
+      resource_name :gcompute_disk_type
+
+      property :creation_timestamp, Time, desired_state: true
+      property :default_disk_size_gb, Integer, desired_state: true
+      property :deprecated_deleted, Time, desired_state: true
+      property :deprecated_deprecated, Time, desired_state: true
+      property :deprecated_obsolete, Time, desired_state: true
+      property :deprecated_replacement, String, desired_state: true
+      property :deprecated_state, String, desired_state: true
+      property :description, String, desired_state: true
+      property :id, Integer, desired_state: true
+      property :name, String, name_property: true, desired_state: true
+      property :valid_disk_size, String, desired_state: true
+      property :zone, String, desired_state: true
+
+      property :credential, String, desired_state: false, required: true
+      property :project, String, desired_state: false, required: true
+
+      action :create do
+        fetch = fetch_resource(@new_resource, self_link(@new_resource),
+                               'compute#diskType')
+        if fetch.nil?
+          converge_by "Creating gcompute_disk_type[#{name}]" do
+            # TODO(nelsonjr): Show a list of variables to create
+            # TODO(nelsonjr): Determine how to print green like update converge
+            puts # making a newline until we find a better way TODO: find!
+            compute_changes.each { |log| puts "    - #{log.strip}\n" }
+            create_req = ::Google::Request::Post.new(collection(@new_resource),
+                                                     fetch_auth(@new_resource),
+                                                     'application/json',
+                                                     resource_to_request)
+            return_if_object create_req.send, 'compute#diskType'
+          end
+        else
+          @current_resource = @new_resource.clone
+          @current_resource.creation_timestamp =
+            ::Google::Property::Time.parse(fetch['creationTimestamp'])
+          @current_resource.default_disk_size_gb =
+            ::Google::Property::Integer.parse(fetch['defaultDiskSizeGb'])
+          @current_resource.deprecated_deleted = ::Google::Property::Time.parse(
+            ::Google::HashUtils.navigate(fetch, %w[deprecated deleted])
+          )
+          @current_resource.deprecated_deprecated =
+            ::Google::Property::Time.parse(
+              ::Google::HashUtils.navigate(
+                fetch, %w[deprecated deprecated]
+              )
+            )
+          @current_resource.deprecated_obsolete =
+            ::Google::Property::Time.parse(
+              ::Google::HashUtils.navigate(
+                fetch, %w[deprecated obsolete]
+              )
+            )
+          @current_resource.deprecated_replacement =
+            ::Google::Property::String.parse(
+              ::Google::HashUtils.navigate(
+                fetch, %w[deprecated replacement]
+              )
+            )
+          @current_resource.deprecated_state = ::Google::Property::Enum.parse(
+            ::Google::HashUtils.navigate(fetch, %w[deprecated state])
+          )
+          @current_resource.description =
+            ::Google::Property::String.parse(fetch['description'])
+          @current_resource.id = ::Google::Property::Integer.parse(fetch['id'])
+          @current_resource.name =
+            ::Google::Property::String.parse(fetch['name'])
+          @current_resource.valid_disk_size =
+            ::Google::Property::String.parse(fetch['validDiskSize'])
+
+          cannot_change_resource 'DiskType cannot be edited'
+        end
+      end
+
+      action :delete do
+        fetch = fetch_resource(@new_resource, self_link(@new_resource),
+                               'compute#diskType')
+        unless fetch.nil?
+          converge_by "Deleting gcompute_disk_type[#{name}]" do
+            delete_req =
+              ::Google::Request::Delete.new(self_link(@new_resource),
+                                            fetch_auth(@new_resource))
+            return_if_object delete_req.send, 'compute#diskType'
+          end
+        end
+      end
+
+      # TODO(nelsonjr): Add actions :manage and :modify
+
+      private
+
+      action_class do
+        def resource_to_request
+          ::Google::HashUtils.camelize_keys(
+            kind: 'compute#diskType'
+          ).reject { |_, v| v.nil? }.to_json
+        end
+
+        def cannot_change_resource(message)
+          converge_if_changed do |_vars|
+            # TODO(nelsonjr): Determine how to print indented like upd converge
+            # TODO(nelsonjr): Check w/ Chef... can we print this in red?
+            puts # making a newline until we find a better way TODO: find!
+            compute_changes.each { |log| puts "    - #{log.strip}\n" }
+            Chef::Log.fatal message
+            raise message
+          end
+        end
+
+        # rubocop:disable Metrics/MethodLength
+        def self.resource_to_hash(resource)
+          {
+            project: resource.project,
+            name: resource.name,
+            kind: 'compute#diskType',
+            creation_timestamp: resource.creation_timestamp,
+            default_disk_size_gb: resource.default_disk_size_gb,
+            deprecated_deleted: resource.deprecated_deleted,
+            deprecated_deprecated: resource.deprecated_deprecated,
+            deprecated_obsolete: resource.deprecated_obsolete,
+            deprecated_replacement: resource.deprecated_replacement,
+            deprecated_state: resource.deprecated_state,
+            description: resource.description,
+            id: resource.id,
+            valid_disk_size: resource.valid_disk_size,
+            zone: resource.zone
+          }.reject { |_, v| v.nil? }
+        end
+        # rubocop:enable Metrics/MethodLength
+
+        # Copied from Chef > Provider > #converge_if_changed
+        def compute_changes
+          properties = @new_resource.class.state_properties.map(&:name)
+          properties = properties.map(&:to_sym)
+          if current_resource
+            compute_changes_for_existing_resource properties
+          else
+            compute_changes_for_new_resource properties
+          end
+        end
+
+        # Collect the list of modified properties
+        def compute_changes_for_existing_resource(properties)
+          specified_properties = properties.select do |property|
+            @new_resource.property_is_set?(property)
+          end
+          modified = specified_properties.reject do |p|
+            @new_resource.send(p) == current_resource.send(p)
+          end
+
+          generate_pretty_green_text(modified)
+        end
+
+        def generate_pretty_green_text(modified)
+          property_size = modified.map(&:size).max
+          modified.map! do |p|
+            properties_str = if @new_resource.sensitive
+                               '(suppressed sensitive property)'
+                             else
+                               [
+                                 @new_resource.send(p).inspect,
+                                 "(was #{current_resource.send(p).inspect})"
+                               ].join(' ')
+                             end
+            "  set #{p.to_s.ljust(property_size)} to #{properties_str}"
+          end
+        end
+
+        # Write down any properties we are setting.
+        def compute_changes_for_new_resource(properties)
+          property_size = properties.map(&:size).max
+          properties.map do |property|
+            default = ' (default value)' \
+              unless @new_resource.property_is_set?(property)
+            next if @new_resource.send(property).nil?
+            properties_str = if @new_resource.sensitive
+                               '(suppressed sensitive property)'
+                             else
+                               @new_resource.send(property).inspect
+                             end
+            ["  set #{property.to_s.ljust(property_size)}",
+             "to #{properties_str}#{default}"].join(' ')
+          end.compact
+        end
+
+        def fetch_auth(resource)
+          self.class.fetch_auth(resource)
+        end
+
+        def self.fetch_auth(resource)
+          resource.resources("gauth_credential[#{resource.credential}]")
+                  .authorization
+        end
+
+        def fetch_resource(resource, self_link, kind)
+          self.class.fetch_resource(resource, self_link, kind)
+        end
+
+        def debug(message)
+          Chef::Log.debug(message)
+        end
+
+        def self.collection(data)
+          URI.join(
+            'https://www.googleapis.com/compute/v1/',
+            expand_variables(
+              'projects/{{project}}/zones/{{zone}}/diskTypes',
+              data
+            )
+          )
+        end
+
+        def collection(data)
+          self.class.collection(data)
+        end
+
+        def self.self_link(data)
+          URI.join(
+            'https://www.googleapis.com/compute/v1/',
+            expand_variables(
+              'projects/{{project}}/zones/{{zone}}/diskTypes/{{name}}',
+              data
+            )
+          )
+        end
+
+        def self_link(data)
+          self.class.self_link(data)
+        end
+
+        def self.return_if_object(response, kind)
+          raise "Bad response: #{response}" \
+            unless response.is_a?(Net::HTTPResponse)
+          return if response.is_a?(Net::HTTPNotFound)
+          return if response.is_a?(Net::HTTPNoContent)
+          result = JSON.parse(response.body)
+          raise_if_errors result, %w[error errors], 'message'
+          raise "Bad response: #{response}" unless response.is_a?(Net::HTTPOK)
+          raise "Incorrect result: #{result['kind']} (expecting #{kind})" \
+            unless result['kind'] == kind
+          result
+        end
+
+        def return_if_object(response, kind)
+          self.class.return_if_object(response, kind)
+        end
+
+        def self.extract_variables(template)
+          template.scan(/{{[^}]*}}/).map { |v| v.gsub(/{{([^}]*)}}/, '\1') }
+                  .map(&:to_sym)
+        end
+
+        def self.expand_variables(template, var_data, extra_data = {})
+          data = if var_data.class <= Hash
+                   var_data.merge(extra_data)
+                 else
+                   resource_to_hash(var_data).merge(extra_data)
+                 end
+          extract_variables(template).each do |v|
+            unless data.key?(v)
+              raise "Missing variable :#{v} in #{data} on #{caller.join("\n")}}"
+            end
+            template.gsub!(/{{#{v}}}/, CGI.escape(data[v].to_s))
+          end
+          template
+        end
+
+        def dirty_display_formatted
+          @dirty.map do |name, change|
+            ["#{name.id2name} expected #{change[:to]},",
+             "but platform reports #{change[:from]}"].join(' ')
+          end.join('. Also ')
+        end
+
+        def self.fetch_resource(resource, self_link, kind)
+          get_request = ::Google::Request::Get.new(self_link,
+                                                   fetch_auth(resource))
+          return_if_object get_request.send, kind
+        end
+
+        def self.raise_if_errors(response, err_path, msg_field)
+          errors = ::Google::HashUtils.navigate(response, err_path)
+          raise_error(errors, msg_field) unless errors.nil?
+        end
+
+        def self.raise_error(errors, msg_field)
+          raise IOError, ['Operation failed:',
+                          errors.map { |e| e[msg_field] }.join(', ')].join(' ')
+        end
+      end
+    end
+    # rubocop:enable Metrics/ClassLength
+  end
+end
