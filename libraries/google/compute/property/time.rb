dd92a6a3b6fa7fd276b69a1be7a010d371b53a3f
Alex Stephen <alexstephen@google.com>
2017-07-28 06:43:05 +0000

    coercing time values to a custom class to ensure iso8601
    All timestamps should be represented by the ISO-8601 standard to
    comply with GCP APIs.
    
    I built this out as a Data:: container that inherits from ::Time after
    some thought. Reasons:
    
    1) Some form of function that coerces values would have to be placed
    into both the providers and inside Nested objects (Nested Objects have
    timestamps that need to be formatted). Currently, we have a clear
    separation between those Data containers and the provider code.
    I feel duplicating this code between them would make that fuzzy.
    
    2) We built out a nice system of not having to do separate logic
    depending on a property type. A function would be the only place where
    we lose that.
    
    3) We want to keep our ::Time objects as ::Times for as long as
    possible. .to_s (or a future .to_resource) should be called at the
    last possible moment. By letting Ruby call it for us, we don't have
    to handle a future instance where some Times are ::Time and others are
    ::Time.iso8601
    
    Change-Id: I1631f6464c87f7525a78e7d90bc09fe4737f731e

--- libraries/google/compute/property/time.rb
+++ libraries/google/compute/property/time.rb
@@ -29,6 +29,20 @@ require 'time'
 
 module Google
   module Compute
+    module Data
+      # A Time that always returns a ISO-8601 string
+      class Time < ::Time
+        def to_s
+          # All GCP APIs expect timestamps in the ISO-8601 / RFC3339 format
+
+          # Overriding the .to_s method ensures that Ruby will get a
+          # ISO-8601 timestamp at the last moment and ensures the timestamp
+          # format is abstracted away.
+          iso8601
+        end
+      end
+    end
+
     module Property
       # A class to handle serialization of Time items.
       class Time
@@ -39,13 +53,13 @@ module Google
         def self.api_parse(value)
           return if value.nil?
           return value if value.is_a? ::Time
-          ::Time.parse(value)
+          Data::Time.parse(value)
         end
 
         def self.catalog_parse(value)
           return if value.nil?
           return value if value.is_a? ::Time
-          ::Time.parse(value)
+          Data::Time.parse(value)
         end
       end
     end
